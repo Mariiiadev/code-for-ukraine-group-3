7e46fed12c10935c18c4ff0c677f0e4cca7ce38b
Mariiiadev <halushkomaria5@gmail.com>
2025-07-23 20:59:37 +0300

    login/sign up page

--- group-3/App.js
+++ group-3/App.js
@@ -5,6 +5,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import AddZbir from "./screens/AddZbir";
+import LogIn from "./screens/LogIn";
+import SignUp from "./screens/SignUp";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -22,8 +24,10 @@ function MainTabs(){
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="MainTabs">
-                <Stack.Screen name="MainTabs" component ={MainTabs} />
+            <Stack.Navigator initialRouteName="LogIn" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="LogIn" component={LogIn} />
+            <Stack.Screen name="MainTabs" component ={MainTabs} />
             </Stack.Navigator>
         </NavigationContainer>
     )
